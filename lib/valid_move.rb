55b34bbf6351e93986e2c3032e42a5bb0830e60c
Morgan Stanley <morganpstanley@gmail.com>
2019-06-20 02:23:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,3 @@
-# code your #valid_move? method here
 def valid_move?(board, index)
   if index.between?(0, 9) && !position_taken?(board, index)
     return true
@@ -7,8 +6,6 @@ def valid_move?(board, index)
   end
 end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if (board[index] == "" || board[index] == " " || board[index] == nil)
     return false
